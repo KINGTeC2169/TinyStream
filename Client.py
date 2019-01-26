bfcc34f59d039031526fe64b64e1bbd2f7a92570
Max DeVos <devosmaxwell@gmail.com>
2019-01-26 11:45:50 -0600

    Networking is hard

--- Client.py
+++ Client.py
@@ -2,15 +2,20 @@
 import cv2
 import numpy as np
 
-
-def showImage(self, encimg):
+def showImage(encimg):
 
     encimg = np.frombuffer(encimg, dtype="uint8")
-
     frame = cv2.imdecode(encimg, 1)
+
+    print(frame)
 
     # Blow the image back up
     newX, newY = frame.shape[1] * 4, frame.shape[0] * 4
     frame = cv2.resize(frame, (int(newX), int(newY)))
 
     cv2.imshow("Frame", frame)
+
+    while True:
+        encimg = np.frombuffer(encimg.decode(), dtype="uint8")
+        frame = cv2.imdecode(encimg, 1)
+        cv2.imshow("Image", frame)
--- Server.py
+++ Server.py
@@ -1,6 +1,6 @@
-import cv2
-import sys
 import pickle
+import sys
+import cv2
 
 import numpy as np
 
@@ -10,23 +10,36 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture("video.mp4")
 
     while True:
-
         # Read a frame from the camera
         ret, frame = cap.read()
 
-        #Make the image 1/4 the size
+        # Make the image 1/4 the size
         newX, newY = frame.shape[1] * .5, frame.shape[0] * .5
         frame = cv2.resize(frame, (int(newX), int(newY)))
 
-        #Set the encode parameters to terrible
+        # Set the encode parameters to terrible
         encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 7]
 
-        #Encode the image into a string
-        results, x = cv2.imencode('.jpg', frame, encode_param)
+        # Encode the image into a string
+        #results, x = cv2.imencode('.jpg', frame, encode_param)
 
-        #Define output string
-        out = x.tostring()
+        enc = pickle.dumps(frame, protocol=2)
+        print(sys.getsizeof(enc))
+        # Define output string
+        # out = x.tostring()
+        # print(type(out))
+        # encimg = np.frombuffer(out, dtype="uint8")
+        # frame = cv2.imdecode(encimg, 1)
+        #
+        # print(out)
+
+        # Blow the image back up
+        newX, newY = frame.shape[1] * 4, frame.shape[0] * 4
+        frame = cv2.resize(frame, (int(newX), int(newY)))
+
+        cv2.imshow("Frame", frame)
 
         k = cv2.waitKey(5) & 0xFF
         if k == 27:
             break
+
